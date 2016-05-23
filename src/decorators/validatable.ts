637822c03e11bfdc5846b7135118af60807dc549
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-05-23 16:42:19 -0700

    q

---
+++ src/decorators/validatable.ts
@@ -0,0 +1,5 @@
+import {objectToArray} from "../utils/utils.ts"
+
+function _getRuleArgs(ruleSpec: any){
+    return objectToArray(ruleSpec.slice(1)); //the first prop of the rule spec is the class of the rule
+}
