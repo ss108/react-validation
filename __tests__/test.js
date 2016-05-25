6cd25fe573585ab85b086f12a7cfa13a8d1843c5
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-05-25 15:59:38 -0700

    learning jest

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,2 +1,13 @@
 jest.unmock('./fx.js');
 
+import fx from "./fx.js";
+
+describe('fx', () =>{
+    it('multiplies input by 2', () =>{
+       var res = fx(2);
+
+       expect(res).toEqual(4);
+    });
+
+});
+
