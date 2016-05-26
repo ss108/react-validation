481a8df413705e38dd554be0427da7a7641c32db
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-05-25 17:45:09 -0700

    apparently this works, for the typings

--- src/decorators/validatable.ts
+++ src/decorators/validatable.ts
@@ -2,6 +2,10 @@ import Q from "q"
 
 import {objectToArray} from "../utils/utils.ts"
 
-function _getRuleArgs(ruleSpec: any){
+function _getRuleArgs(ruleSpec: any): Array<any>{
     return objectToArray(ruleSpec.slice(1)); //the first prop of the rule spec is the class of the rule
+}
+
+function _satisfiesRule(c: React.Component<any, any>): boolean{
+
 }
